0b691d8377381d4cc91a2ea0d4a2efe9326169b3
gottagoa <136464755+gottagoa@users.noreply.github.com>
2023-12-25 16:34:37 +0600

    creating graphics

--- scraping/multi_channels.py
+++ scraping/multi_channels.py
@@ -31,4 +31,13 @@ def get_channel_stats(youtube, channel_ids):
 
 channel_statistics=get_channel_stats(youtube, channel_ids)
 channel_data=pd.DataFrame(channel_statistics)
-print(channel_data)
+# print(channel_data)
+# print(channel_statistics)
+# channel_data['Subscribers']=pd.to_numeric(channel_data['Subscribers'])
+# channel_data['Views']=pd.to_numeric(channel_data['Views'])
+channel_data['Total_videos']=pd.to_numeric(channel_data['Total_videos'])
+# print(channel_data)
+sns.set(rc={'figure.figsize':(10,8)})
+ax=sns.barplot(x='Channel_name', y='Subscribers', data=channel_data)
+# показывает в виде графика у кого больше всего подписчиков
+print(ax)
